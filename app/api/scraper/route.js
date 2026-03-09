8e4b5298296120280cc327545e6a8bba2b0c5a5c
LORD PREMO <cypherx599@gmail.com>
2026-03-09 11:58:59 +0300

    Update route.js

--- app/api/scraper/route.js
+++ app/api/scraper/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { scrapeWebsite } from "@/lib/scraper";
+import { scrapeWebsite } from "../../../../lib/scraper";
 
 export async function POST(request) {
   try {
